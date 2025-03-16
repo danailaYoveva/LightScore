f138b7997fdd1234aa112be744c73ec4ccc2ef64
thristovaa <raya.thristova@gmail.com>
2025-03-16 13:51:58 +0100

    login page

--- src/main/java/nl/saxion/re/sponsorrun/controllers/MainMenuController.java
+++ src/main/java/nl/saxion/re/sponsorrun/controllers/MainMenuController.java
@@ -1,18 +1,37 @@
 package nl.saxion.re.sponsorrun.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import nl.saxion.re.sponsorrun.util.WindowHelper;
 
 public class MainMenuController {
     @FXML
+    public VBox vbox;
+
+    @FXML
     public ImageView imageView;
+
+    @FXML
+    public TextField nameField;
+
+    @FXML
+    public TextField passwordField;
 
     @FXML
     public void initialize() {
         imageView.setImage(new Image(getClass().getResource("/image/logo.png").toExternalForm()));
+
+        nameField.setPromptText("Username");
+        passwordField.setPromptText("Password");
+
+        vbox = new VBox();
+
+        Scene scene = new Scene(vbox, 750, 600);
     }
 
     @FXML
