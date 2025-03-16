a1c12e4dfd04091e39f452bab77859f47bb230a2
thristovaa <raya.thristova@gmail.com>
2025-03-16 13:03:16 +0100

    logo

--- src/main/java/nl/saxion/re/sponsorrun/controllers/MainMenuController.java
+++ src/main/java/nl/saxion/re/sponsorrun/controllers/MainMenuController.java
@@ -2,9 +2,20 @@ package nl.saxion.re.sponsorrun.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import nl.saxion.re.sponsorrun.util.WindowHelper;
 
 public class MainMenuController {
+    @FXML
+    public ImageView imageView;
+
+    @FXML
+    public void initialize() {
+        imageView.setImage(new Image(getClass().getResource("/image/logo.png").toExternalForm()));
+    }
+
+
     @FXML
     private Label welcomeText;
 
